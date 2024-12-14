4b15fe6d7cbbbf123b09e3bb0616ab83453fa5e3
chavely15k <chavely15k@gmail.com>
2024-12-14 15:23:05 -0500

    cleaning implementation

--- Topics/SparseTable/sparse_table.cpp
+++ Topics/SparseTable/sparse_table.cpp
@@ -3,38 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
-
-#define SPEED                \
-    ios::sync_with_stdio(0); \
-    cin.tie(0)
-
-#define reverse(x) reverse(x.begin(), x.end())
-
-#define print(x) cout << x << '\n'
-
 #define all(v) v.begin(), v.end()
-
-#define mod(x) x %= (int)(1e9 + 7)
-
-#define rep(a, b) for (int i = a; i < b; i++)
-#define per(a, b) for (int i = b - 1; i >= a; i--)
-#define repi(i, a, b) for (int i = a; i < b; i++)
-#define peri(i, a, b) for (int i = b - 1; i >= a; i--)
-
-#define NO cout << "NO\n"
-#define YES cout << "YES\n"
-#define NEW_LINE cout << '\n'
-
-#define dx {+1, +0, -1, +0, +1, +1, -1, -1}
-#define dy {+0, +1, +0, -1, +1, -1, +1, -1}
-
-#define ALPHABET "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-#define alphabet "abcdefghijklmnopqrstuvwxyz"
-#define decimal_digits "0123456789"
 
 #pragma endregion
 
